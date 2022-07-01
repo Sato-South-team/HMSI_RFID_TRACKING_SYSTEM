3281235bb52056cfa5224a018d2e82e8d106eeab
sar.puttaraju.ah <sar.puttaraju.ah@SAR-N-PF2795MB>
2022-07-01 13:07:48 +0530

    Add project files.

---
+++ HMSI_RFID_TRACKING_SYSTEM/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HMSI_RFID_TRACKING_SYSTEM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
